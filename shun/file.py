e62880b3ccb54d338b1679668c00f6c346bb3f90
Ishaan1106 <shunsuke062004@gmail.com>
2024-04-10 18:14:57 +0530

    ishaan

--- shun/file.py
+++ shun/file.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
